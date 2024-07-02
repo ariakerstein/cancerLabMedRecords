7fdd0887e0991f251689e5c704542fd8c3d747ad
ari akerstein <ariakerstein@aris-MacBook-Air.local>
2024-07-02 11:33:04 -0400

    update credentials

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,18 +1,27 @@
 import { Title } from '@mantine/core';
 import { Logo, SignInForm } from '@medplum/react';
 import { useNavigate } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 export function SignInPage(): JSX.Element {
   const navigate = useNavigate();
   return (
     <SignInForm
       // Configure according to your settings
-      googleClientId="921088377005-3j1sa10vr6hj86jgmdfh2l53v3mp7lfi.apps.googleusercontent.com" // use this one to run against prod
+      googleClientId="const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;" // use this one to run against prod
       // googleClientId="397236612778-c0b5tnjv98frbo1tfuuha5vkme3cmq4s.apps.googleusercontent.com" // use this one for localhost
       onSuccess={() => navigate('/')}
     >
       <Logo size={32} />
       <Title>Sign in to Medplum</Title>
     </SignInForm>
+  );
+}
+
+function App() {
+  return (
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      {/* Your app components */}
+    </GoogleOAuthProvider>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import dns from 'dns';
-
-dns.setDefaultResultOrder('verbatim');
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: 'localhost',
-    port: 3000,
-  },
+  define: {
+    'process.env': {}
+  }
 });
